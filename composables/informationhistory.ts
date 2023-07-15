08c99119574e629d2b5f1625af7d84c7390515ff
motoo1789 <motoo1789@gmail.com>
2023-07-16 01:19:31 +0900

    add ２ページで新着情報を表示することができる

--- composables/informationhistory.ts
+++ composables/informationhistory.ts
@@ -1,46 +1,57 @@
-const {$client} = useNuxtApp()
-const id = "4aRueTYaQFRMv4drT6irTy";
-//const { data:cms } = await useAsyncData(id ,()=> $client.getEntries())
-// const { data:cms } = await useAsyncData(id ,()=> $client.getEntry(id))
-// const { data:cms } = await useAsyncData(id ,()=> $client.getEntries({content_type: 'newInformation'}))
-// const { data:getContents } = await useAsyncData( () => $client.getEntries())
-
-
-export const getCMSContent = async () => {
-
+export const getCMSContents = async () => {
     const {$client} = useNuxtApp()
     const { data:getContents } = await useAsyncData( () => $client.getEntries())
     
     const contents = getContents.value?.items;
     const parseContents = contents.map(item => item.fields);
-    return parseContents;
-
+    return parseContents
 };
 
-// export const getCMSContent = () => {
-//   const getContent =   async () => {
-//     const {$client} = useNuxtApp()
-//     const { data:getContents } = await useAsyncData( () => $client.getEntries())
-    
-//     const contents = getContents.value?.items;
-//     const parseContents = contents.map(item => item.fields);
-//     console.log(parseContents);
-//     return parseContents;
-    
-//   };
-//   return { 
-//     getContent
-//   }
-// }
+export const getCMSContent = () => {
+    const cmsComtents = useState("cmsComtents" , () => getCMSContents() );
+    return { 
+        cmsComtents: readonly(cmsComtents),
+    }
+}
 
 export const getUpdateInformation = () => { 
-  const parseUpdateInformation = async () => {
-    const  parsebeforeCMSContents  = await getCMSContent();
-    return parsebeforeCMSContents
-  };
-  return {
-    parseUpdateInformation
-  }
+    const parseUpdateInformation = async () => {
+        const { cmsComtents } = await getCMSContent()
+
+        const contentsValueObject = await cmsComtents.value;
+        console.log(contentsValueObject)
+        const hobbyArray = contentsValueObject?.filter(cmsContent => {
+            return cmsContent.hasOwnProperty('updateDate')
+        })
+        const afterSortHobbyArray = hobbyArray.sort((a,b) => Date.parse(b.updateDate) - Date.parse(a.updateDate));
+
+
+        const showMaxInformation = 5;
+        const returnPageObject = []
+        const contentLength = afterSortHobbyArray.length
+
+        for(let counterPage = 1; counterPage <= Math.ceil(contentLength / showMaxInformation); counterPage++)
+        {
+            const onePageInformation = afterSortHobbyArray.slice(0, showMaxInformation);
+            const pushPageCounterInformationContent = {
+                page: counterPage,
+                contents: onePageInformation,
+            }
+            returnPageObject.push(pushPageCounterInformationContent)
+            if(afterSortHobbyArray.length > 5)
+            {
+                for(let counterShift = 0; counterShift < showMaxInformation; counterShift++)
+                {
+                    afterSortHobbyArray.shift();
+                }
+            }
+            
+        }
+        return returnPageObject
+    };
+    return {
+        parseUpdateInformation
+    }
 }
 
 export const updatePage = (currentpage: Ref<number>) => {
@@ -66,8 +77,6 @@ export const useCurrentPage = () => {
 }
 
 export const useInformationHisotry = () => {
-
-
     const hisotory = useState('hisotory', () => {
         return [
             {
