1201f1c0e11fd11a8d7c0270bfad6a7461dd7141
motoo1789 <motoo1789@gmail.com>
2023-08-06 23:59:37 +0900

    bcrypt用いてハッシュ生成

--- server/api/account/addUser.ts
+++ server/api/account/addUser.ts
@@ -1,4 +1,4 @@
-
+import bcrypt from "bcrypt";
 
 export default defineEventHandler(async (event) => {
     console.log("サーバー側処理：addUser")
@@ -8,6 +8,18 @@ export default defineEventHandler(async (event) => {
         const body = await readBody(event);
 
         console.log(body)
+
+        const password = body.password;
+        const saltRounds = 10
+
+        const salt = bcrypt.genSaltSync(saltRounds)
+        console.log('salt: ', salt)
+        
+        const hash = bcrypt.hashSync(password, salt)
+        console.log('hash: ', hash)
+        
+        console.log('data1: ', bcrypt.compareSync(password, hash))
+        console.log('data2: ', bcrypt.compareSync("dte", hash))
 
         return "success";
     }
