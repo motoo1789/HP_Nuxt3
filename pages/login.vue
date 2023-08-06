13448aff148991523046a22c892a0b51b3cd18c2
motoo1789 <motoo1789@gmail.com>
2023-08-06 12:41:38 +0900

    メールアドレスでログイン用のフォーム作成中

--- pages/login.vue
+++ pages/login.vue
@@ -2,8 +2,10 @@
 <template>
 	<div class="align-center justify-center" style="height: 100vh">
         <v-sheet width="400" class="mx-auto">
-            <v-form fast-fail @submit.prevent="login">
-                <v-text-field  variant="underlined" v-model="username" label="User Name"></v-text-field>
+            <pre>{{ status }}</pre>
+            <pre>{{ data }}</pre>
+            <v-form fast-fail @submit.prevent="loginMailPass">
+                <v-text-field  variant="underlined" v-model="email" label="User Mail"></v-text-field>
 
                 <v-text-field variant="underlined" v-model="password" label="Password"></v-text-field>
                 
@@ -13,11 +15,24 @@
         </v-sheet>
 
 		<v-btn @click="signIn('github')" type="submit" variant="outlined" color="light-blue-accent-3" block class="mt-2">Githubでログイン</v-btn>
+
+    <NuxtLink :to="`/AddAcount`" class="link-style-home ">
+      <v-btn  type="submit" variant="outlined" color="light-blue-accent-3" block class="mt-2 " >
+        アカウント登録
+      </v-btn>
+    </NuxtLink>
     </div>
 </template>
 
 <script lang="ts" setup>
-const { signIn } = useAuth()
+const { status, data, signIn } = useAuth()
+
+const email = ref("");
+const password = ref("");
+
+const loginMailPass = async () => {
+  await signIn('credentials', { email: email.value, password: password.value })
+}
 
 definePageMeta( {
 	auth: {
--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,4 +1,5 @@
 import GithubProvider from "next-auth/providers/github"
+import CredentialsProvider from 'next-auth/providers/credentials'
 
 import { NuxtAuthHandler } from "#auth";
 
@@ -8,7 +9,22 @@ export default NuxtAuthHandler({
         GithubProvider.default({
             clientId: process.env.GITHUB_CLIENT_ID!,
             clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-        })
+        }),
+        CredentialsProvider.default({
+            authorize(credentials: { email: string, password: string }) {
+              if(credentials.email === "motoo1798@gmail.com")
+              {
+                return {
+                    email: credentials.email,
+                    password: credentials.password,
+                }
+              }
+              else {
+                console.error('Warning: Malicious login attempt registered, bad credentials provided');
+                return null;
+              }
+            }
+          })
     ]
 })
 
