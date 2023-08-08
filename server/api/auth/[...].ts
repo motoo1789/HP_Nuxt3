088863d97e0d06e53ba05d78b23a0b493aebe362
motoo1789 <motoo1789@gmail.com>
2023-08-09 00:30:40 +0900

    user_id get

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -2,6 +2,7 @@ import GithubProvider from "next-auth/providers/github"
 import CredentialsProvider from 'next-auth/providers/credentials'
 
 import { NuxtAuthHandler } from "#auth";
+import { sql } from '@vercel/postgres';
 
 export default NuxtAuthHandler({
     providers: [
@@ -11,8 +12,17 @@ export default NuxtAuthHandler({
             clientSecret: process.env.GITHUB_CLIENT_SECRET!,
         }),
         CredentialsProvider.default({
-            authorize(credentials: { email: string, password: string }) {
-              if(credentials.email === "motoo1798@gmail.com")
+             async authorize(credentials: { email: string, password: string }) {
+
+              console.log(credentials.email);
+              console.log(`SELECT user_id FROM users WHERE email = '${credentials.email}'`)
+              const { rows, fields } = await sql`SELECT user_id FROM users WHERE email = ${credentials.email}`;
+              console.log("user id get")
+              const db_user_id = rows[0].user_id;
+              
+              console.log(db_user_id);
+
+              if(credentials.email === "motoo1789@gmail.com")
               {
                 return {
                     email: credentials.email,
