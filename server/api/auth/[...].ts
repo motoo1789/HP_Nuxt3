fa24ace95b360b6eff0b262497372c8e668552f4
motoo1789 <motoo1789@gmail.com>
2023-08-10 00:04:54 +0900

    add logun feature

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -2,6 +2,7 @@ import GithubProvider from "next-auth/providers/github"
 import CredentialsProvider from 'next-auth/providers/credentials'
 
 import { NuxtAuthHandler } from "#auth";
+import bcrypt from "bcrypt";
 import { sql } from '@vercel/postgres';
 
 export default NuxtAuthHandler({
@@ -12,29 +13,53 @@ export default NuxtAuthHandler({
             clientSecret: process.env.GITHUB_CLIENT_SECRET!,
         }),
         CredentialsProvider.default({
-             async authorize(credentials: { email: string, password: string }) {
+			async authorize(credentials: { email: string, password: string }) {
 
-              console.log(credentials.email);
-              console.log(`SELECT user_id FROM users WHERE email = '${credentials.email}'`)
-              const { rows, fields } = await sql`SELECT user_id FROM users WHERE email = ${credentials.email}`;
-              console.log("user id get")
-              const db_user_id = rows[0].user_id;
-              
-              console.log(db_user_id);
+				const dbUserId:string = await getEmailUser(credentials.email);
+				console.log(dbUserId);
+				if(dbUserId === "No acount")
+				{
+					console.log("アカウントが存在しません");
+					return {
+						id: "id",
+						name: 'name',
+						username: 'username',
+						email: credentials.email,
+						message: "アカウントが存在しません"
+					}
+				}
 
-              if(credentials.email === "motoo1789@gmail.com")
-              {
-                return {
-                    email: credentials.email,
-                    password: credentials.password,
-                }
-              }
-              else {
-                console.error('Warning: Malicious login attempt registered, bad credentials provided');
-                return null;
-              }
-            }
-          })
+				const correctPassword:boolean = await checkPassword(dbUserId, credentials.password);
+
+				if(correctPassword)
+				{
+					console.log("存在確認")
+					return true
+				}
+				else {
+					//console.error('Warning: Malicious login attempt registered, bad credentials provided');
+					return false
+					
+				}
+        	}
+        })
     ]
 })
 
+const getEmailUser = async ( email:string ): Promise<string> => 
+{
+	const { rows, fields } = await sql`SELECT user_id FROM users WHERE email = ${email}`;
+	console.log("ここまではくるよね");
+	return rows[0]?.user_id ?? "No acount";
+}
+
+const checkPassword = async ( dbUserId:string, password:string ): Promise<boolean> => 
+{
+	console.log(`SELECT * FROM password_security_info WHERE usre_id = ${dbUserId}`)
+	const { rows, fields } = await sql`SELECT * FROM password_security_info where user_id = ${dbUserId}`;
+	console.log("sql後")
+
+	const hash:string = rows[0]?.password_hash ?? "no";
+	
+	return bcrypt.compareSync(password, hash);
+}
