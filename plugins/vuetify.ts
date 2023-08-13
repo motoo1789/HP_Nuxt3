a6995fa4129bc4db908077024e26a880a06a3db6
motoo1789 <motoo1789@gmail.com>
2023-08-13 20:46:45 +0900

    vuetifyを3.3.12へアップ，vuepic

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,12 +1,18 @@
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import { VuetifyDateAdapter } from 'vuetify/labs/date/adapters/vuetify'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
     components,
     directives,
+    date: {
+      adapter: VuetifyDateAdapter,
+    },
   });
+  
 
   nuxtApp.vueApp.use(vuetify);
 });
+
