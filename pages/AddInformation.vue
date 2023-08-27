75680813a1f1c48844d8cab5d8bcbaa948fdbfb1
motoo1789 <motoo1789@gmail.com>
2023-08-27 18:36:56 +0900

    infoの追加時のショートメッセージ追加

--- pages/AddInformation.vue
+++ pages/AddInformation.vue
@@ -1,7 +1,7 @@
 
 <template>
 	<div>
-		
+
 		<v-container>
 			<v-row>
 				<v-col>
@@ -11,57 +11,58 @@
 			</v-row>
 			<v-row>
 				<v-col>
-					
+
 					<form @submit="onSubmit">
 						<!--タイトル-->
-						
+
 
 						<!--概要-->
-						<v-alert variant="text" type="warning" density="compact" v-if="errors.abstract" id="error-name-required"
-							aria-live="assertive">
+						<v-alert variant="text" type="warning" density="compact" v-if="errors.abstract"
+							id="error-name-required" aria-live="assertive">
 							概要が未入力です！
 						</v-alert>
 						<v-text-field label="概要" name="abstract" v-model="abstract"></v-text-field>
 
 						<!--本文-->
-						<v-alert variant="text" type="warning" density="compact" v-if="errors.detaile" id="error-name-required"
-							aria-live="assertive">
+						<v-alert variant="text" type="warning" density="compact" v-if="errors.detaile"
+							id="error-name-required" aria-live="assertive">
 							本文が未入力です！
 						</v-alert>
 						<v-textarea label="本文" name="detaile" v-model="detaile"></v-textarea>
 
 						<!--作成日時-->
-						<v-alert variant="text" type="warning" density="compact" v-if="errors.createdInformationDate" id="error-name-required"
-							aria-live="assertive">
+						<v-alert variant="text" type="warning" density="compact" v-if="errors.createdInformationDate"
+							id="error-name-required" aria-live="assertive">
 							作成日時が未入力です！
 						</v-alert>
-						<v-text-field
-							label="作成日時"
-							prepend-inner-icon="mdi-calendar-cursor"
-							name="date"
-							v-model="createdInformationDate"
-							@click="dateDialog = true"
-						></v-text-field>
+						<v-text-field label="作成日時" prepend-inner-icon="mdi-calendar-cursor" name="date"
+							v-model="createdInformationDate" @click="dateDialog = true"></v-text-field>
 
 						<v-dialog v-model="dateDialog" width="auto">
-							<v-defaults-provider >
+							<v-defaults-provider>
 								<v-locale-provider locale="ja">
-									<v-date-picker v-model="createdInformationDate" format="yyyy-dd-mm" ></v-date-picker>
+									<v-date-picker v-model="createdInformationDate" format="yyyy-dd-mm"></v-date-picker>
 								</v-locale-provider>
 							</v-defaults-provider>
-							
 
-							<v-btn 
-								color="light-blue-accent-3" 		
-								@click="dateDialog = false"
-							>
+
+							<v-btn color="light-blue-accent-3" @click="dateDialog = false">
 								閉じる
 							</v-btn>
 						</v-dialog>
-						
+
 						<div class="text-center justify-center">
 							<v-btn class="me-4" type="submit" color="light-blue-accent-3">送信</v-btn>
 						</div>
+
+						<v-snackbar min-width="auto" width="150" :timeout="3500" v-model="notifySuccess"
+							color="light-blue-accent-3">
+							<p class="text-center">送信成功！</p>
+						</v-snackbar>
+
+						<v-snackbar min-width="auto" width="150" :timeout="3500" v-model="notifyError" color="error">
+							<p class="text-center">送信失敗！</p>
+						</v-snackbar>
 					</form>
 				</v-col>
 
@@ -87,22 +88,22 @@ import * as yup from "yup"
 import { DatePickerType } from 'vuetify';
 
 const schema = yup.object({
-	abstract:   yup.string().required(),
-	detaile:  yup.string().required(),
-	createdInformationDate:  yup.string().required(),
+	abstract: yup.string().required(),
+	detaile: yup.string().required(),
+	createdInformationDate: yup.string().required(),
 });
 
 const { useFieldModel, handleSubmit, errors } = useForm({
 	validationSchema: schema
 })
-const [abstract, detaile,createdInformationDate] 
-		= useFieldModel(['abstract', 'detaile','createdInformationDate'])
+const [abstract, detaile, createdInformationDate]
+	= useFieldModel(['abstract', 'detaile', 'createdInformationDate'])
 
 watch(createdInformationDate, (newCreatedProjectDate, oldCreatedProjectDate) => {
-    // .valueは不要
-    console.log("old ", oldCreatedProjectDate) 
-    console.log("new ", newCreatedProjectDate)
-	createdInformationDate.value = <string> newCreatedProjectDate.replace('UTC', '');
+	// .valueは不要
+	console.log("old ", oldCreatedProjectDate)
+	console.log("new ", newCreatedProjectDate)
+	createdInformationDate.value = <string>newCreatedProjectDate.replace('UTC', '');
 })
 
 interface POSTFormat {
@@ -111,23 +112,19 @@ interface POSTFormat {
 	createdInformationDate: string,
 }
 
+let notifySuccess = ref(false)
+let notifyError = ref(false);
+
 const onSubmit = handleSubmit(async (values) => {
 
-	// const formData = new FormData()
-	// Object.entries(values).forEach(([key, value]) => {
-	// 	console.log(value)
-	// 	formData.append(key, value)
-	// })
 	const formData = {
 		abstract: values.abstract,
 		detaile: values.detaile,
 		createdInformationDate: values.createdInformationDate,
 	}
-	
 
 	try {
-		console.log(formData);
-		const response = useFetch ("/api/contentful/PostInformation", {
+		const response = useFetch("/api/contentful/PostInformation", {
 			method: 'POST',
 			body: formData,
 			headers: {
@@ -135,11 +132,11 @@ const onSubmit = handleSubmit(async (values) => {
 			}
 		})
 
-		if (response.data.value === 'success') {
-			await navigateTo('/thanks')
+		if (response.data.value === "success") {
+			notifySuccess.value = true;
 		} else {
+			notifyError.value = true;
 			console.log(response)
-			// await navigateTo('/error')
 		}
 	} catch (err) {
 		await navigateTo('/error')
