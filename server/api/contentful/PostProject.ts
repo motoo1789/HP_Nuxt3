01a8e8b9814b04f86ce7ed60ca07f086d257da2f
motoo1789 <motoo1789@gmail.com>
2023-09-02 23:37:00 +0900

    ファイルの書き込みがうまくいかない

--- server/api/contentful/PostProject.ts
+++ server/api/contentful/PostProject.ts
@@ -1,31 +1,32 @@
 import contentful from "contentful-management";
 import { Environment } from "contentful-management/dist/typings/entities/environment";
+import path from "path";
+import fs from "fs";
 
 interface POSTFormat {
-	title: string,
-	abstract: string,
-	detail: string,
-	library: string
-	language: string,
-	framework: string,
-	github: string,
-	createdProjectDate: string,
-	image: string,
-	movie: string,
+    title: string;
+    abstract: string;
+    detail: string;
+    library: string;
+    language: string;
+    framework: string;
+    github: string;
+    createdProjectDate: string;
+    image: string;
+    movie: string;
 }
 
 export default defineEventHandler(async (event) => {
-    console.log("サーバー側処理：addUser")
-    
+    console.log("サーバー側処理：addUser");
+
+
     // const postBody = await readMultipartFormData(event);
     // console.log("readMultipartFormData");
     // console.log(postBody);
 
-
-
     console.log("readData");
     //console.log(await readBody(event) as POSTFormat) ;
-    const post = await readBody(event) as POSTFormat;
+    const post = (await readBody(event)) as POSTFormat;
 
     const image = post.image;
     const movie = post.movie;
@@ -36,23 +37,31 @@ export default defineEventHandler(async (event) => {
     // console.log(fetchimage.body);
 
     //const blob = new Blob([fetchimage.respons], { type: "image/jpeg" });
-    const imageFileData = image.replace(/^data:\w+\/\w+;base64,/, '')
-    const movieFileData = movie.replace(/^data:\w+\/\w+;base64,/, '')
+    const imageFileData = image.replace(/^data:\w+\/\w+;base64,/, "");
+    const movieFileData = movie.replace(/^data:\w+\/\w+;base64,/, "");
     // console.log(fileData)
-    const decodedImageFile = Buffer.from(imageFileData, 'base64')
-    const decodedMovieFile = Buffer.from(movieFileData, 'base64')
+    const decodedImageFile = Buffer.from(imageFileData, "base64");
+    const decodedMovieFile = Buffer.from(movieFileData, "base64");
 
     // ファイルの拡張子(png)
-    const imageFileExtension = image.toString().slice(image.indexOf('/') + 1, image.indexOf(';'))
-    const movieFileExtension = movie.toString().slice(movie.indexOf('/') + 1, movie.indexOf(';'))
-
+    const imageFileExtension = image
+        .toString()
+        .slice(image.indexOf("/") + 1, image.indexOf(";"));
+    const movieFileExtension = movie
+        .toString()
+        .slice(movie.indexOf("/") + 1, movie.indexOf(";"));
 
     // ContentType(image/png)
-    const contentImageType = image.toString().slice(image.indexOf(':') + 1, image.indexOf(';'))
-    const contentMovieType = movie.toString().slice(movie.indexOf(':') + 1, movie.indexOf(';'))
+    const contentImageType = image
+        .toString()
+        .slice(image.indexOf(":") + 1, image.indexOf(";"));
+    const contentMovieType = movie
+        .toString()
+        .slice(movie.indexOf(":") + 1, movie.indexOf(";"));
 
-
-    const contentfullPostFile = new File([decodedImageFile], 'postimage.png', { type: 'image/png' })
+    // const contentfullPostFile = new File([decodedImageFile], "postimage.png", {
+    //     type: "image/png",
+    // });
     // console.log("decodedFile" )
     // console.log(typeof decodedFile )
     // console.log(decodedFile )
@@ -67,46 +76,119 @@ export default defineEventHandler(async (event) => {
 
     const sendImageContentful = {
         Body: decodedImageFile,
-        Bucket: 'Bucket Name',
-        Key: [post.title, imageFileExtension].join('.'),
-        ContentType: 'application/octet-stream'
-    }
+        Bucket: "Bucket Name",
+        Key: [post.title, imageFileExtension].join("."),
+        ContentType: "application/octet-stream",
+    };
     const sendMovieContentful = {
         Body: decodedMovieFile,
-        Bucket: 'Bucket Name',
-        Key: [post.title, movieFileExtension].join('.'),
-        ContentType: 'application/octet-stream'
-    }
-    
+        Bucket: "Bucket Name",
+        Key: [post.title, movieFileExtension].join("."),
+        ContentType: "application/octet-stream",
+    };
 
-    //console.log(sendImageContentful)    
+    console.log("if前")
+    const tmpfileimagename = [post.title, imageFileExtension].join(".");
+    const tmpfilemoviname = [post.title, movieFileExtension].join(".");
 
-    if (event.node.req.method === 'POST') {
+    if (event.node.req.method === "POST") {
+        console.log("postif直後");
+        await fs.writeFileSync(`./public/movie/${tmpfileimagename}`, imageFileData, {
+            encoding: "base64",
+        });
+        await fs.writeFileSync(`./public/movie/${tmpfilemoviname}`, movieFileData, {
+            encoding: "base64",
+        });
+        const tmpImageFilePath = await path.resolve("./public/image", tmpfileimagename);
+        const tmpImageFile = await fs.readFileSync(tmpImageFilePath);
+
+        const tmpMovieFilePath = await path.resolve("./public/movie", tmpfilemoviname);
+        const tmpMovieFile = await fs.readFileSync(tmpMovieFilePath);
+
+        
         const client = contentful.createClient({
             accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_MANAGEMENT_API_KEY!,
-            host: "api.contentful.com" // ホストは共通なので.envに記載しない
+            host: "api.contentful.com", // ホストは共通なので.envに記載しない
+        });
+        const mySpace = await client.getSpace(
+            process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID!
+        );
+        const myEnvironment = await mySpace.getEnvironment(
+            process.env.NEXT_PUBLIC_CONTENTFUL_ENVIROMENT!
+        );
+        const uploadImage = await myEnvironment.createUpload({
+            file: tmpImageFile,
+        });
+        const uploadMovie = await myEnvironment.createUpload({
+            file: tmpMovieFile,
         });
 
-        const mySpace = await client.getSpace(process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID!);
-        const myEnvironment = await mySpace.getEnvironment(process.env.NEXT_PUBLIC_CONTENTFUL_ENVIROMENT!);
-        const assetRes = await myEnvironment.createAsset({
-            fields: {
-                title: {
-                'en-US': 'sample'
+        const assetImage = await myEnvironment
+            .createAsset({
+                fields: {
+                    title: {
+                        "en-US": post.title,
+                    },
+                    file: {
+                        "en-US": {
+                            contentType: "image/jpg",
+                            fileName: `${post.title}`,
+                            uploadFrom: {
+                                sys: {
+                                    type: "Link",
+                                    linkType: "Upload",
+                                    id: uploadImage.sys.id,
+                                },
+                            },
+                        },
+                    },
                 },
-                description: {
-                'en-US': 'sample'
-                },
-                file: {
-                'en-US': {
-                    contentType: 'image/jpeg',
-                    fileName: 'example.jpeg',
-                    upload: decodedImageFile,
-                }
-                }
-            }
-        })
+            })
+            .then((asset) => {
+                return asset.processForAllLocales({
+                    processingCheckWait: 2000,
+                });
+            })
+            .then((asset) => {
+                return asset.publish();
+            });
 
+        const assetMovie = await myEnvironment
+            .createAsset({
+                fields: {
+                    title: {
+                        "en-US": post.title,
+                    },
+                    file: {
+                        "en-US": {
+                            contentType: "image/jpg",
+                            fileName: `${post.title}`,
+                            uploadFrom: {
+                                sys: {
+                                    type: "Link",
+                                    linkType: "Upload",
+                                    id: uploadImage.sys.id,
+                                },
+                            },
+                        },
+                    },
+                },
+            })
+            .then((asset) => {
+                return asset.processForAllLocales({
+                    processingCheckWait: 2000,
+                });
+            })
+            .then((asset) => {
+                return asset.publish();
+            });
+
+        await fs.unlink(`./public/image/${tmpfileimagename}`, (err) => {
+            if (err) throw err;
+        });
+        await fs.unlink(`./public/movie/${tmpfilemoviname}`, (err) => {
+            if (err) throw err;
+        });
         // const mySpace = await client.getSpace(process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID!);
         // const myEnvironment = await mySpace.getEnvironment(process.env.NEXT_PUBLIC_CONTENTFUL_ENVIROMENT!);
         // const assetRes = await myEnvironment.createEntry(process.env.NEXT_PUBLIC_CONTENTFUL_CONTENT_TYPE_PROJECT!,{
@@ -165,12 +247,9 @@ export default defineEventHandler(async (event) => {
         //         }
         //     }
         // });
-            
+
         // assetRes.publish();
         // console.log("投稿OK")
         return "success";
     }
-
-
-
-})
+});
