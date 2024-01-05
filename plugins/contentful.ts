436187a777516a7ac73cc29c8b5f00ed994f4103
motoo1789 <motoo1789@gmail.com>
2024-01-05 22:26:32 +0900

    fix: contentfulのコンテンツ修正
    - pluginsの中で環境変数の値を取得しcontentfulに接続

--- plugins/contentful.ts
+++ plugins/contentful.ts
@@ -1,35 +1,13 @@
 import { createClient } from "contentful";
 
-interface Contentful {
-  contentfulSpaceId? : string;
-  contentfulAccessToken? : string;
-}
-
 export default defineNuxtPlugin(() => {
 
   const configContentful = useRuntimeConfig();
-  console.log(configContentful);
-
-  const contentful : Contentful = {
-    contentfulSpaceId: configContentful.public.contentfulSpaceId,
-    contentfulAccessToken: configContentful.public.contentfulAccessToken
-  }
-
-  console.log("テスト");
-  console.log("configContentful.public.contentfulAccessToken");
-  console.log(configContentful.public.contentfulAccessToken);
-  console.log("configContentful.public.contentfulSpaceId");
-  console.log(configContentful.public.contentfulSpaceId);
-
-  console.log(createClient({
-    space: configContentful.public.contentfulSpaceId,
-    accessToken: configContentful.public.contentfulAccessToken
-  }));
 
   if( configContentful.public.contentfulSpaceId     === undefined ||
       configContentful.public.contentfulAccessToken === undefined) 
   {
-    console.log("undefindらしい");
+    console.log("コンテンツを取得できていません");
     return {provide: {
       client: createClient({
         space: "",
