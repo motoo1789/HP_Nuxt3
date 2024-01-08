3fafe53525e498a797bc0e2dd60a9e98300f0bdd
motoo1789 <motoo1789@gmail.com>
2024-01-08 21:09:45 +0900

    不要な出力文を削除

--- plugins/contentful.ts
+++ plugins/contentful.ts
@@ -1,55 +1,31 @@
 import { createClient } from "contentful";
-import { CreateClientParams } from 'contentful'
-import { ContentfulClientApi } from "contentful"
+import { CreateClientParams } from "contentful";
 
 export default defineNuxtPlugin(async () => {
-
   const configContentful = await useRuntimeConfig();
 
-  if(configContentful.public.contentfulSpaceId === undefined) {
-    throw new Error("error");
-    // return { provide : {client: createClient({space: "", accessToken: ""})}};
+  if (	configContentful.public.contentfulSpaceId 		=== undefined ||
+    		configContentful.public.contentfulAccessToken === undefined) {
+    return {
+      provide: {
+        client: createClient({
+          space: "",
+          accessToken: "",
+        }),
+      },
+    };
   }
 
-  if(configContentful.public.contentfulAccessToken === undefined) {
-    throw new Error("error");
-    // return { provide : {client: createClient({space: "", accessToken: ""})}};
-  }
-
-  console.log(`env アクセストークン:${configContentful.public.contentfulAccessToken}`)
-  console.log("env アクセストークン:CRWQWaVfuYO7GFoepi_x8EJlCPEKc8gRXx9-u9kE_ds")
-  if(configContentful.public.contentfulAccessToken === 'CRWQWaVfuYO7GFoepi_x8EJlCPEKc8gRXx9-u9kE_ds')
-{
-  console.log("同じ1")
-}
-if(configContentful.public.contentfulSpaceId === 'gtch6w6darsf')
-{
-  console.log("同じ2")
-}
-
-  const test : CreateClientParams = {
-    space : configContentful.public.contentfulSpaceId,
-    accessToken : configContentful.public.contentfulAccessToken
-  }
+  const test: CreateClientParams = {
+    space: configContentful.public.contentfulSpaceId,
+    accessToken: configContentful.public.contentfulAccessToken,
+  };
 
   const client = await createClient(test);
-  console.log(client);
-
-  // if( configContentful.public.contentfulSpaceId     === undefined ||
-  //     configContentful.public.contentfulAccessToken === undefined) 
-  // {
-  //   console.log("コンテンツを取得できていません");
-  //   return {provide: {
-  //     client: createClient({
-  //       space: "",
-  //       accessToken: ""
-  //     })
-  //   }}
-  // }
 
   return {
     provide: {
-      client: client
-    }
-  }
+      client: client,
+    },
+  };
 });
