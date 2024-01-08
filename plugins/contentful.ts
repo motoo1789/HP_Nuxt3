88edc4ecd43b621038023a56c97ee11eae83411f
motoo1789 <motoo1789@gmail.com>
2024-01-08 20:52:46 +0900

    vercelに格納していたアクセストークンが正しくない

--- plugins/contentful.ts
+++ plugins/contentful.ts
@@ -29,7 +29,7 @@ if(configContentful.public.contentfulSpaceId === 'gtch6w6darsf')
 
   const test : CreateClientParams = {
     space : configContentful.public.contentfulSpaceId,
-    accessToken : 'CRWQWaVfuYO7GFoepi_x8EJlCPEKc8gRXx9-u9kE_ds'
+    accessToken : configContentful.public.contentfulAccessToken
   }
 
   const client = await createClient(test);
