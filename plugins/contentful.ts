a0994b7de8515a2eda474e5d947a8bd90e4c4364
motoo1789 <motoo1789@gmail.com>
2024-01-08 18:05:41 +0900

    fix: エラーはくように

--- plugins/contentful.ts
+++ plugins/contentful.ts
@@ -3,9 +3,20 @@ import { createClient } from "contentful";
 export default defineNuxtPlugin(async () => {
 
   const configContentful = await useRuntimeConfig();
+
+  if(configContentful.public.contentfulSpaceId === undefined) {
+    throw new Error("error");
+    // return { provide : {client: createClient({space: "", accessToken: ""})}};
+  }
+
+  if(configContentful.public.contentfulAccessToken === undefined) {
+    throw new Error("error");
+    // return { provide : {client: createClient({space: "", accessToken: ""})}};
+  }
+
   const client = await createClient({
-    space: `${configContentful.public.contentfulSpaceId}`,
-    accessToken: `${configContentful.public.contentfulAccessToken}`
+    space: configContentful.public.contentfulSpaceId,
+    accessToken: configContentful.public.contentfulAccessToken
   });
   console.log(client);
 
