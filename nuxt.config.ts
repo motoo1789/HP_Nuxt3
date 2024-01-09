3eae40b5df256bbf32387ffa7c1bb666544c4b14
motoo1789 <motoo1789@gmail.com>
2024-01-09 22:38:04 +0900

    public じゃなきゃダメ見たい

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,9 @@
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
-     
-    },
-    private: {
       contentfulSpaceId: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
       contentfulAccessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
-    }
+    },
   },
 
   configureWebpack: {
