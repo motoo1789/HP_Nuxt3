5962276fe642d56dba98186f29c5fde5965029b6
motoo1789 <motoo1789@gmail.com>
2024-02-11 22:57:15 +0900

    prd マージ用

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -57,7 +57,7 @@ export default defineNuxtConfig({
 
   auth: {
     enableGlobalAppMiddleware: false, // trueの場合は最初にログインページを表示
-    // origin: 'https://hp-nuxt3-motoo1789.vercel.app'
+    origin: 'https://hp-nuxt3-motoo1789.vercel.app'
   },
 
   redirect: {
