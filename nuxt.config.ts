1f4635686424f921ce1f15b8144825b728da4148
motoo1789 <motoo1789@gmail.com>
2024-08-02 23:24:45 +0900

    add: aws file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -70,7 +70,7 @@ export default defineNuxtConfig({
 
   auth: {
     enableGlobalAppMiddleware: false, // trueの場合は最初にログインページを表示
-    origin: 'https://motoo1789.net/' // vercelのデプロイに必要
+    // origin: 'https://motoo1789.net/' // vercelのデプロイに必要
   },
 
   redirect: {
