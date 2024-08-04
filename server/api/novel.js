9cf8a105c905480c1045b36ce189d600f8c8bd3f
motoo1789 <motoo1789@gmail.com>
2024-08-04 22:51:16 +0900

    delete: Delete unused files

--- server/api/novel.js
+++
@@ -1,7 +0,0 @@
-import fs from "fs";
-import path from 'path';
-
-export default defineEventHandler(async (event) => {
-    const articles = JSON.parse(fs.readFileSync('assets/hobby_novel.json', 'utf-8')).novel;
-    return articles;
-  });
