39be068d72da9a61209a7c64d13a3cb3e122d39b
motoo1789 <motoo1789@gmail.com>
2024-08-08 23:05:41 +0900

    add: vitestサンプル実行用コード

---
+++ tests/sample.test.ts
@@ -0,0 +1,8 @@
+import { describe, test, expect } from "vitest";
+
+describe("addnumber", () => {
+    test("足し算のテストサンプル", () => {
+        expect(undefined).toEqual(undefined);
+        
+    })
+})
